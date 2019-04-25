66f1f2e5401fa36b8fd29eba3f021042d47d555d
Daniel Gatto <d.mario.gatto@gmail.com>
2019-04-25 19:40:08 +0930

    for realsies?

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -4,8 +4,14 @@ param (
     [Parameter(Mandatory = $true,
         HelpMessage = "Enter the path of the registry values CSV file.")][string]$RegValuesCsvPath,
     [Parameter(Mandatory = $true,
-        HelpMessage = "Enter the path of the choco packages CSV file.")][string]$ChocoPackagesCsvPath
+        HelpMessage = "Enter the path of the choco packages CSV file.")][string]$ChocoPackagesCsvPath,
+    [Parameter(Mandatory = $true,
+        HelpMessage = "For realsies?")][bool]$ForRealsies
 )
+
+if ($ForRealsies) {
+    Set-TimeZone -Name "Cen. Australia Standard Time"
+}
 
 # Set Registry Entries
 Import-Csv $RegValuesCsvPath -delimiter '|' |
@@ -16,11 +22,13 @@ ForEach-Object {
         $val = $_.Value
         $type = $_.Type
         
-        if (!(Test-Path $registryPath)) { 
-            #New-Item -Path $registryPath -Force | Out-Null 
+        if (!(Test-Path $registryPath) -and $ForRealsies) { 
+            New-Item -Path $registryPath -Force | Out-Null 
         }
 
-        #New-ItemProperty -Path $registryPath -Name $name -Value $val -PropertyType $type -Force | Out-Null
+        if ($ForRealsies) {
+            New-ItemProperty -Path $registryPath -Name $name -Value $val -PropertyType $type -Force | Out-Null
+        }
         
         if ($_.Description) {
             Write-Host $_.Description
@@ -31,17 +39,26 @@ ForEach-Object {
     }    
 }
 
+# Restart Explorer 💣
+if ($ForRealsies) {
+    Stop-Process -ProcessName explorer
+}
+
 # Choco Time 🍫
 if (Get-Command "choco" -errorAction SilentlyContinue) {
     Write-Host "Checking for Chocolatey update..."
-    choco upgrade chocolatey -y
+    if ($ForRealsies) {
+        choco upgrade chocolatey -y
+        choco feature enable --name=useRememberedArgumentsForUpgrades
+    }
 }
 else {
     Write-Host "Installing Chocolatey..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    if ($ForRealsies) {
+        Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        choco feature enable --name=useRememberedArgumentsForUpgrades
+    }
 }
-
-choco feature enable --name=useRememberedArgumentsForUpgrades
 
 Import-Csv $ChocoPackagesCsvPath -delimiter '|' |
 ForEach-Object {
@@ -49,11 +66,20 @@ ForEach-Object {
         $packName = $_.Package
         if ($_.Parameters) { 
             $params = $_.Parameters
-            Write-Host "choco install $packName $params -y"
+            if ($ForRealsies) {
+                choco install $packName $params -y
+            }
+            else {
+                Write-Host "choco install $packName $params -y"
+            }
         }
         else { 
-            Write-Host "choco install $packName -y"
+            if ($ForRealsies) {
+                choco install $packName -y
+            }
+            else {
+                Write-Host "choco install $packName -y"
+            }
         }
     }    
 }
-
