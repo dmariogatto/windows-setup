870e7eb4ad84546473663f537509c6fc4580d3f1
Daniel Gatto <d.mario.gatto@gmail.com>
2019-04-29 17:05:25 +0930

    choco version checking

--- windows-init.ps1
+++ windows-init.ps1
@@ -11,7 +11,7 @@ Set-TimeZone -Name "Cen. Australia Standard Time"
 
 # Set Registry Entries
 if ($RegValsPath -and (Test-Path $RegValsPath)) {
-    Import-Csv $RegValsPath -delimiter '|' |
+    Import-Csv $RegValsPath -Delimiter '|' |
     ForEach-Object {
         if ($_.Path -and $_.Name -and $_.Value -and $_.Type) {
             $registryPath = $_.Path
@@ -49,14 +49,15 @@ else {
 if ($ChocoPackagesPath -and (Test-Path $ChocoPackagesPath)) {
     $chocoInstalled = Get-Command "choco" -errorAction SilentlyContinue
 
-    if ($chocoInstalled) {
-        Write-Host "Chocolatey installation found..."
-    }
-
     if ($CommitChoco) {
         if ($chocoInstalled) {
-            Write-Host "Checking for Chocolatey update..."
-            choco upgrade chocolatey -y
+            Write-Host "Chocolatey installation found..."
+            $currentVer = New-Object System.Version(choco -v)       
+            $requiredVer = New-Object System.Version("0.10.4")    
+            if ($currentVer -lt $requiredVer) {
+                Write-Host "Chocolatey version $requiredVer or greater required, triggering update..."
+                choco upgrade chocolatey -y
+            }
         }
         else {
             Write-Host "Installing Chocolatey..."
@@ -66,21 +67,45 @@ if ($ChocoPackagesPath -and (Test-Path $ChocoPackagesPath)) {
         choco feature enable --name=useRememberedArgumentsForUpgrades
     }   
 
-    Import-Csv $ChocoPackagesPath -delimiter '|' |
+    $installedPackages = @{ };
+    choco list --lo -r | ConvertFrom-Csv -Header Package, Version -Delimiter '|' |
+    ForEach-Object {
+        if ($_.Package) {
+            $installedPackages[$_.Package] = $_.Version
+        }    
+    }
+
+    $chocoInstallCmds = @();
+    Import-Csv $ChocoPackagesPath -Delimiter '|' |
     ForEach-Object {
         if ($_.Package) {
             $packName = $_.Package
             $params = $_.Parameters
-            $installArgs = if ($params) { "$packName $params -y" } else { "$packName -y" }
-            $chocoCommand = "choco install $installArgs"
-            Write-Host $chocoCommand
-            
-            if ($CommitChoco) {
-                Invoke-Expression $chocoCommand
+
+            $currentVersion = $installedPackages[$packName]
+            if ($currentVersion) {
+                Write-Host "Package $packName ($currentVersion) is already installed, skipping install..."                
             }
+            else {
+                $installArgs = if ($params) { "$packName $params -y" } else { "$packName -y" }
+                $chocoCommand = "choco install $installArgs -r"
+                $chocoInstallCmds += $chocoCommand
+            }           
         }    
+    }
+
+    $chocoInstallCmds |
+    ForEach-Object {
+        if ($CommitChoco) {
+            Write-Host $_
+            Invoke-Expression $_
+        }
+        else {
+            Write-Host $_
+        }
     }
 }
 else {
     Write-Host "Skipping choco..."
 }
+
